4e628c9156e35de13af5d88c7b840fcec25fc6e3
Abegg <Christian.Abegg@zuehlke.com>
2016-11-08 11:37:35 +0100

    inital commit

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+// Rollup plugins
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+
+export default {
+  entry: 'src/app.js',
+  dest: 'dist/app.js',
+  format: 'iife',
+  sourceMap: true,
+  plugins: [
+
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+
+    commonjs({
+      include: 'node_modules/**'
+    }),
+
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ],
+};
